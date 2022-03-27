12d7c7a0d324b5004d71749d66efb613af52e621
tekac <soehnerj@gmail.com>
2022-03-26 23:17:22 -0400

    barebones structuring and design of the calculator - will beging javascript logic

---
+++ script.js
@@ -0,0 +1,24 @@
+const add = (n1, n2) => n1 + n2;
+const subtract = (n1, n2) => n1 - n2;
+const multiply = (n1, n2) => n1 * n2;
+const divide = (n1, n2) => n1 / n2;
+
+const operate = (operand, n1, n2) => {
+  let value = 0;
+
+  switch (operand) {
+    case "+":
+      value = add(n1, n2);
+      break;
+    case "-":
+      value = subtract(n1, n2);
+      break;
+    case "*":
+      value = multiply(n1, n2);
+      break;
+    case "/":
+      value = divide(n1, n2);
+      break;
+  }
+  return value;
+};
